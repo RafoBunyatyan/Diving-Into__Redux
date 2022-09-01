b76c93d8950ba79439707331f97a1764c4bfe7a2
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-09-01 18:14:50 +0400

    last commit

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,5 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const initialAuthState = {
+	isAuthenticated: false
+}
+
 const authSlice = createSlice({
 	name: 'authentication',
 	initialState: initialAuthState,
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counter'
 import authReducer from './auth'
 
-const initialCounterState = { counter: 0, showCounter: true }
-
-const initialAuthState = {
-	isAuthenticated: false
-}
 
 const store = configureStore({
 	reducer: { counter: counterReducer, auth: authReducer }
