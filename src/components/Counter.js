06a85f009ec6674770b6773cd3ec701804eb3e38
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-09-10 16:26:14 +0400

    first commit ))

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -48,9 +48,7 @@ export default Counter
 // 		this.props.decrement()
 // 	}
 
-// 	toggleCounterHandler() {
-
-// 	}
+// 	toggleCounterHandler() {}
 
 // 	render() {
 // 		return (
--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counter'
 import authReducer from './auth'
 
-
 const store = configureStore({
 	reducer: { counter: counterReducer, auth: authReducer }
 })
